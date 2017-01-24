400fc51523ccd21e5c58ac257944bd67508bb180
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2017-01-24 16:47:14 -0500

    Initial Commit

---
+++ algorithmic-toolbox/src/main/java/coursera/week1/MaxPairwiseProduct.java
@@ -0,0 +1,23 @@
+/*
+ * MaxPairwiseProduct.java
+ *
+ * Jan 24, 2017
+ *
+ * Copyright (c) Smartmatic Corp.
+ * 1001 Broken Sound Parkway NW, Suite D
+ * Boca Raton FL 33487, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and propietary information of
+ * Smartmatic Corp. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with Smartmatic Corp.
+ */
+package coursera.week1;
+
+public class MaxPairwiseProduct {
+
+
+
+}
---
+++ hacker-earth/src/main/java/searching/LinearSearch.java
@@ -0,0 +1,26 @@
+/*
+ * LinearSearch.java
+ *
+ * Jan 19, 2017
+ *
+ * Copyright (c) Smartmatic Corp.
+ * 1001 Broken Sound Parkway NW, Suite D
+ * Boca Raton FL 33487, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and propietary information of
+ * Smartmatic Corp. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with Smartmatic Corp.
+ */
+package searching;
+
+/**
+ * @author Ernesto Olivo   (ernesto.olivo@smartmatic.com).
+ * @version 6.0.1.SL (Jan 19, 2017).
+ */
+public class LinearSearch {
+
+
+}
