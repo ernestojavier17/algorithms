5353a5f72033c859c65f0ecf0adf24221a19548f
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2018-01-23 18:28:23 -0500

    Course Specialization

---
+++ hearth/src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package sorting;
+
+
+public class SelectionSort {
+
+    public void sort(int[] array) {
+        int minIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+    }
+
+    void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+}
