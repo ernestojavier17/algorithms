dc008e004a73713cec0d6dc95967e80d331993a2
Ernesto Javier <olivo.ernesto@gmail.com>
2018-02-06 08:14:41 -0500

    Recursion Start

---
+++ hearth/src/test/java/recursion/PalindromeTest.java
@@ -0,0 +1,56 @@
+package recursion;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PalindromeTest {
+
+    private Palindrome palindrome;
+
+    @Before
+    public void setUp() {
+        palindrome = new Palindrome();
+    }
+
+    @Test
+    public void isPalindrome_TrueOddWord() {
+        Assert.assertTrue(palindrome.isPalindrome("rotor"));
+    }
+
+    @Test
+    public void isPalindrome_TrueEvenWord() {
+        Assert.assertTrue(palindrome.isPalindrome("redder"));
+    }
+
+    @Test
+    public void isPalindrome_False() {
+        Assert.assertFalse(palindrome.isPalindrome("motor"));
+    }
+
+
+    @Test
+    public void isPalindromeR_TrueOddWord() {
+        Assert.assertTrue(palindrome.isPalindromeR("rotor".toCharArray(), 0));
+    }
+
+    @Test
+    public void isPalindromeR_TrueEvenWord() {
+        Assert.assertTrue(palindrome.isPalindromeR("redder".toCharArray(), 0));
+    }
+
+    @Test
+    public void isPalindromeR_False() {
+        Assert.assertFalse(palindrome.isPalindromeR("motor".toCharArray(), 0));
+    }
+
+    @Test
+    public void isPalindromeR_TrueOneLetter() {
+        Assert.assertTrue(palindrome.isPalindromeR("m".toCharArray(), 0));
+    }
+
+    @Test
+    public void isPalindromeR_TrueOddLongWord() {
+        Assert.assertTrue(palindrome.isPalindromeR("rootoor".toCharArray(), 0));
+    }
+}
